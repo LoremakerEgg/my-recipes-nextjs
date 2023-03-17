be5b809a18b2cc3d91485fe46e677cde67c2039e
Anton <antonkoller@outlook.com>
2023-03-17 11:45:41 +0100

    Added not

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>My Recipes</title>
+        <title>Not My Recipes</title>
         <meta name="description" content="Welcome to my recipes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
